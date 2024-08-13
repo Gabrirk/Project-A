bc11c08e5d8b8662a701af428343537cf795b26a
Gabriel Koehler <gabrielrk1999@gmail.com>
2024-08-13 16:35:03 -0300

    GridManager
    grid and tiles prefab created

---
+++ Project A/Assets/Scripts/GridManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int widht, height;
+    [SerializeField] private Tile tilePrefab;
+    [SerializeField] private Transform cam;
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        for (int x = 0; x < widht; x++) {
+            for (int y = 0; y < height; y++) {
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+
+                cam.transform.position = new Vector3((float)widht / 2 - 0.5f, (float)height / 2 - 0.5f, -10);
+
+
+
+            }
+        }
+
+
+
+    }
+
+
+}
